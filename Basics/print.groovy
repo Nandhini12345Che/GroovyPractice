dfd463b44f6849f05f27bb1021b00200a1c9900c
Nandhini12345Che <148053565+Nandhini12345Che@users.noreply.github.com>
2023-10-16 16:13:15 +0530

    commit
    second change

--- Basics/print.groovy
+++ Basics/print.groovy
@@ -1,1 +1,6 @@
-print "Hello world"
+class print{
+    static Void main (String[] arg){
+        // Using a simple println statement to prnt output to the console
+        println('Hai ');
+    }
+}
