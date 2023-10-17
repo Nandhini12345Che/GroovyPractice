b349ae9f9d7e3e95c7ba46dda2ba43dc5366d482
Nandhini12345Che <148053565+Nandhini12345Che@users.noreply.github.com>
2023-10-17 10:22:07 +0530

    commit
    new Changes

---
+++ Basics/Array1.groovy
@@ -0,0 +1,6 @@
+class Array1{
+    static void main(String[] args){       
+    def arr=[1,2,3,12,43,12,9,24,4,10]
+    println("List of Array :" +arr)
+     }
+     }
---
+++ Basics/LargestArray.groovy
@@ -0,0 +1,7 @@
+class LargestArray{
+    static void main(String[] args){       
+    def arr=[1,2,3,12,43,12,9,24,4,10]
+        def largest=arr.max();
+        println("The largest number is  "+largest);
+    }
+}
