a3277a850094107d92c9883ad5c019adb47297ec
Nandhini12345Che <148053565+Nandhini12345Che@users.noreply.github.com>
2023-10-18 15:02:40 +0530

    commit
    Reverse of number

---
+++ Basics/ReverseNumber.groovy
@@ -0,0 +1,12 @@
+class ReverseNumber {
+   static void main(String[] args) {
+    int num=4567
+    int rev=0
+    while(num !=0){
+        int digit=num %10;
+        rev=rev *10 + digit
+        num=num/10
+    }
+    print(rev)
+   }
+}
