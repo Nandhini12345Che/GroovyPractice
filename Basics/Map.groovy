202c3596fc55347cf5c759b035dc4969648a9547
Nandhini12345Che <148053565+Nandhini12345Che@users.noreply.github.com>
2023-10-18 14:26:27 +0530

    commit
    Map and set

---
+++ Basics/Map.groovy
@@ -0,0 +1,16 @@
+class Map{
+    static Void main (String[] arg){
+        //defining map key and value pair
+        //map declared by def keyword followed by variable name
+        //key must be unique and values will accept duplicates
+        def map = [EmployeeName: "Nandhini",EmpID:1001,Role: "Developer"];
+        //creating map
+        map.put("DOj","jan-2023")
+         //fetching map
+        println(map)  
+         //declaring using set keyword followed by variable name
+        //values must be unique dublicate values will not be accepted
+         Set  num=[1,10,7,4,5,6,12,8,9,5,10]
+           println(num)
+    }
+}
